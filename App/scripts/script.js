ed9a2b2195605d244b2d5948733e5810436bba6b
Alex Peters <petersa3@lopers.unk.edu>
2025-12-11 07:45:53 -0600

    Remove own code
    Changed my mind on the JS, indecisive

--- App/scripts/script.js
+++ App/scripts/script.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', async () => {
     
-    
-    
-    
     // initial variable declaration
     const key = "278985C45F75E625318400A6BC376ACD";
 
-    // Start of Regular JS
+    // Page Nav functionality
     const initialScreen = document.querySelector("#page1");
     const dataScreen = document.querySelector("#page2");
     const enter = document.querySelector("#idSubmit");
@@ -130,15 +127,4 @@ async function FetchAPI(link){
     } catch(error){
         console.log(error);
     }
-}
-
-
-
-
-
-
-
-// html generation functions
-async function loadGame() {
-
 }
