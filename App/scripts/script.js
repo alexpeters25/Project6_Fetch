9da2c96a9b8d6b2eaee1b9561ce3d7fed625f870
Alex Peters <petersa3@lopers.unk.edu>
2025-12-11 17:27:16 -0600

    loadGameAccordion() and loadGameAchievements()
    Created the JS functions that generate the html, set their attributes, and append children up the hierarchy. Both still need sections for setting the actual data

--- App/scripts/script.js
+++ App/scripts/script.js
@@ -128,3 +128,193 @@ async function FetchAPI(link){
         console.log(error);
     }
 }
+
+
+
+
+
+//html generation
+function loadGameAccordion(gamesArray, u1, u2) { //still needs data to be set
+    const gameCount = 0;
+    const divFrameBody2 = document.querySelector("#frameBody2"); //need to clear this div after the first button press
+    for (game of gamesArray.length) {
+        if (gameCount < gamesArray.length) { 
+            //create elements
+            const divL0 = document.createElement("div");
+            const divL1 = document.createElement("div");
+            const divL2 = document.createElement("div");
+            
+            const h2L3 = document.createElement("h2");
+            const buttonL4 = document.createElement("button");
+
+            const divL3 = document.createElement("div");
+            const divL4 = document.createElement("div");
+
+            const divL5Playtime = document.createElement("div");
+            const pL6a = document.createElement("p");
+            const pL6b = document.createElement("p");
+            const pL6c = document.createElement("p");
+
+            const divL5AchvCount = document.createElement("div");
+            const pL6d = document.createElement("p");
+
+            const divL6CountBox = document.createElement("div");
+            const pL7a = document.createElement("p");
+            const pL7b = document.createElement("p");
+            const pL7c = document.createElement("p");
+
+            const divL5AchvAccordion = document.createElement("div");
+            const divL6AchvAccordionItem = document.createElement("div");
+
+
+            const h4L7 = document.createElement("h4");
+            const buttonL8 = document.createElement("button");
+
+            const divL7 = document.createElement("div");
+            const divL8 = document.createElement("div");
+            
+
+            //set data
+            loadGameAchievements(achievementsArray.game, divL8)
+
+            
+            //set classes and id's
+            divL0.className = "accordion";
+            divL0.id = "accordionParent" + gameCount;
+
+
+            divL1.className = "gameBox"
+
+
+            divL2.classList.add("accordion-item", "primaryTextColor");
+
+
+            h2L3.classList.add("accordion-header", "gametitleRibbon");
+            h2L3.id = "heading" + gameCount;
+
+            buttonL4.classList.add("accordion-button", "collapsed");
+            buttonL4.type = "button"
+            buttonL4.setAttribute("data-bs-toggle", "collapse");
+            buttonL4.setAttribute("data-bs-target", ("collapse" + gameCount));
+            buttonL4.setAttribute("aria-expanded", "true");
+            buttonL4.setAttribute("aria-controls", ("collapse" + gameCount));
+
+
+            divL3.classList.add("accordion-collapse", "collapse");
+            divL3.id = "collapse" + gameCount;
+            divL3.setAttribute("aria-labelledby", ("heading" + gameCount));
+            divL3.setAttribute("data-bs-parent", ("accordionParent" + gameCount));
+
+
+            divL4.classList.add("accordion-body", "gameBoxBody");
+
+
+            divL5Playtime.className = "playtime";
+
+
+            divL5AchvCount.className = "achievementCount";
+
+            divL6CountBox.className = "achievementCountBox";
+
+
+            divL5AchvAccordion.className = "accordion";
+            divL5AchvAccordion.id = "achvAccordionParent" + gameCount;
+
+
+            divL6AchvAccordionItem.classList.add("accordion-item", "primaryTextColor");
+
+
+            h4L7.className = "accordion-header";
+            h4L7.id = "achvHeading" + gameCount;
+
+            buttonL8.classList.add("accordion-button", "collapsed");
+            buttonL8.type = "button"
+            buttonL8.setAttribute("data-bs-toggle", "collapse");
+            buttonL8.setAttribute("data-bs-target", ("collapseAchv" + gameCount));
+            buttonL8.setAttribute("aria-expanded", "true");
+            buttonL8.setAttribute("aria-controls", ("collapseAchv" + gameCount));
+
+
+            divL7.classList.add("accordion-collapse", "collapse");
+            divL7.id = "collapseAchv" + gameCount;
+            divL7.setAttribute("aria-labelledby", ("achvHeading" + gameCount));
+            divL7.setAttribute("data-bs-parent", ("achvAccordionParent" + gameCount));
+
+
+            divL8.classList.add("accordion-body", "achievementListBox");
+
+
+
+            //place elements
+            h4L7.appendChild(buttonL8);
+            divL7.appendChild(divL8);
+            
+            divL6AchvAccordionItem.appendChild(h4L7);
+            divL6AchvAccordionItem.appendChild(divL7);
+
+            divL5AchvAccordion.appendChild(divL6AchvAccordionItem);
+
+
+            divL6CountBox.appendChild(pL7a);
+            divL6CountBox.appendChild(pL7b);
+            divL6CountBox.appendChild(pL7c);
+
+            divL5AchvCount.appendChild(pL6d);
+            divL5AchvCount.appendChild(divL6CountBox);
+
+
+            divL5Playtime.appendChild(pL6a);
+            divL5Playtime.appendChild(pL6b);
+            divL5Playtime.appendChild(pL6c);
+
+
+            divL4.appendChild(divL5Playtime);
+            divL4.appendChild(divL5AchvCount);
+            divL4.appendChild(divL5AchvAccordion);
+
+
+            h2L3.appendChild(buttonL4);
+
+            divL3.appendChild(divL4);
+
+
+            divL2.appendChild(h2L3);
+            divL2.appendChild(divL3);
+
+            divL1.appendChild(divL2);
+            divL0.appendChild(divL1);
+
+            divFrameBody2.appendChild(divL0);
+        }
+    }
+}
+
+function loadGameAchievements(achievementsArray, gameDivL8) {
+    for (achv of achievementsArray.length) {
+        const achvIconURL = achv.icon //whatever the attribute is to retrieve the achievement's icon
+        //create elements
+        const divL0 = document.createElement("div");
+        
+        const iconImg = document.createElement("img");
+        const achvName = document.createElement("p");
+        const yourCompletion = document.createElement("img");
+        const otherCompletion = document.createElement("img");
+
+        //set data
+        achvIcon.src = achvIconURL;
+        achvIcon.alt = "icon";
+
+        
+        //set class
+        divL0.className = "listedAchievement";
+
+        //place in top div
+        divL0.appendChild(iconImg);
+        divL0.appendChild(achvName);
+        divL0.appendChild(yourCompletion);
+        divL0.appendChild(otherCompletion);
+
+        //place in level 8 game div
+        gameDivL8.appendChild(divL0);
+    }
+}
